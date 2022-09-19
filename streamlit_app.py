44c30cd6876aec8eed048c88b46b261dc02a1f40
Zhou Nan <zhounan@1utar.my>
2022-09-19 10:46:15 +0800

    :heavy_plus_sign:

---
+++ streamlit_app.py
@@ -0,0 +1,80 @@
+#%%
+import numpy as np
+import pandas as pd
+from io import BytesIO
+from xlsxwriter import Workbook
+from pyxlsb import open_workbook as open_xlsb
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib import font_manager
+import seaborn as sns
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.decomposition import PCA, TruncatedSVD
+from collections import Counter, OrderedDict
+import copy as cp
+from sklearn.decomposition import LatentDirichletAllocation as LDiA
+import gensim
+from PIL import Image
+import streamlit as st
+import altair as alt
+#%%
+# Path: V-TCM\streamlit_app.py
+# 全局设置
+# streamlit
+tab1, tab2, tab3, tab4, tab5, tab6,tab7,tab8 = st.tabs(
+    ["Descriptive statistics", "Prescription similarity", "herbal generality",
+     "LSA topic distribution", "LDiA topic distribution","word embedding", "Download",
+     "About the program"])
+st.set_page_config(layout="wide")
+# matplotlib
+font = font_manager.FontProperties(fname='simhei.ttf')
+parameters = {'xtick.labelsize': 20,
+              'ytick.labelsize': 20,
+              'font.family':'SimHei',
+              'axes.unicode_minus':False}
+plt.rcParams.update(parameters)
+fontsize = 30
+
+#%%
+# 定义文件转换函数
+class conver:
+    "Convert xlsx files"
+    def __int__(self, file):
+        self.file = file
+    def to_xlsx(self):
+        buffer = BytesIO()
+        xls= pd.ExcelFile(self.file)
+        with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+            xls.to_excel(writer, sheet_name='Sheet1')
+            writer.save()
+            processed_data = buffer.getvalue()
+        return processed_data
+#%%
+# 读取示例数据
+out1 = pd.read_excel('English example.xlsx')
+out2 = pd.read_excel('中文示例.xlsx')
+out1 = out1.set_index('Prescription name')
+out2 = out2.set_index('方剂名称')
+out1 = conver(out1).to_xlsx()
+out2 = conver(out2).to_xlsx()
+english_example = pd.read_excel(out1)
+chinese_example = pd.read_excel(out2)
+
+#%%
+# %%
+# 侧栏上传文件区域
+with st.sidebar:
+    file = st.file_uploader("Click “Browse files” to upload files", type=["xlsx"])
+    st.write('Please upload a file no larger than 200MB')
+    st.write('The file must be a .xlsx file')
+    st.download_button('Download sample data in English', data=english_example, file_name='sample data in English.xlsx',
+                       )
+    st.download_button('下载中文示例数据', data=chinese_example, file_name='中文示例数据.xlsx')
+    st.write('Note: You can understand the workflow of this program by uploading sample data.')
+    st.write(
+        'Note: When the program is running, there will be a little man doing sports in the upper right corner of the web page,don\`t refresh this page or do anything else until he stops.')
+
+
+
+
+
